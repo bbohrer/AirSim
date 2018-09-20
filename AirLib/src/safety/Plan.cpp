8f98c349580477bb452e5a5d2c609a8ee538c1c4
Brandon Bohrer <bbohrer@cs.cmu.edu>
2018-09-20 10:44:42 -0400

    Initial version of planner, update map

---
+++ AirLib/src/safety/Plan.cpp
@@ -0,0 +1,194 @@
+/* 
+ * The Plan represents all the places that the vehicle wishes to go in the future.
+ * It is represented as a directed graphs where the vertices are waypoints with radii.
+ * The radius determines the max safe velocity within that waypoint.
+ * The edges are the intersections between adjacent waypoint circles; their maximum safe velocity is determined 
+ * by the radius of the largest inscribed circle of the this intersection.
+ * (this relationship stems from the fact that braking and steering are used for safety and that braking and steering effectiveness
+ *  depend on velocity)
+ * Generally speaking, a larger number of circles means that these intersections are also larger so the speed limits are higher.
+ * However, a large number of circles also demands a faster control cycle so that we do not, say, pass through 3 circles in the plan
+ * by the next time the controller loops.
+ * One of our goals in configuration is to set the fidelty on the plan so that it exploits available processor speed without surpassing it.
+ * The update rate of sensors is also relevant, but in cases where the processor is significantly faster than the sensors,
+ * estimation can be used to run the controller at higher rate
+ * 
+ * Different graph structures represent different styles of driving.
+ * A chain graph represents a deterministic plan, where the only flexibility provided to the controller is which part of
+ * the waypoint circle it drives through (this certain level of control is necessary to account for sensor/actuator imperfection and for imperfect computations in
+ * monitors). 
+ * Any other DAG with a common sink represents a "go-to" style mission with added robustness: the controller is free to pick between multiple paths, say
+ * when one of them is determined to be blocked or dangerous
+ * Any other DAG with multiple sinks represents a "go to any of these places" style mission, arguably the least useful.
+ * Cyclic graphs are quite useful as they represent continuous patrol missions. As with go-to missions, they can have branches
+ * to provide robustness.
+ *
+ * TODO (decreasing priority):
+ *  Basic structure for storing plans
+ *  Derived computations: speed limits, connectivity, cyclicity
+ *  High-level utilities (draw line, draw arc, draw splin)
+ *  Connect to frame rate
+ *  Build appropriate environment for this
+ *  Build controller around this
+ *  Feasability /robustness analysis
+ *  Estimators
+ *  Frequency-of-visit data for better nondeterministic patrols
+ */
+
+//#include "CarPawnSimApi.h"
+#include "AirBlueprintLib.h"
+#include "UnrealSensors/UnrealSensorFactory.h"
+//#include "CarPawnApi.h"
+#include <exception>
+#include <set>
+
+extern float GAverageFPS = 0.0f;
+
+using namespace msr::airlib;
+
+struct NodeDatum {
+	double x;   // meters from global Unreal origin
+	double y;   // meters from global Unreal origin
+	double rad; // meters
+};
+
+struct Mob { // Mobile entity
+	double x;
+	double y;
+	double vx;
+	double vy;
+};
+
+class Plan {
+public:
+	void setMob(double x, double y, double vx, double vy) {
+		m_vehicle = { x,y,vx,vy };
+	}
+	void jumpMob(double x, double y) {
+		m_vehicle = { x,y,0,0 };
+	}
+	int addNode(NodeDatum nd) { return addNode(vector<int>(),nd); }
+	int addNode(int pred, NodeDatum nd) { return addNode(vector<int>(pred),nd); }
+	int addNode(vector<int> preds, NodeDatum nd) {
+		int thisNode = m_nodeCount++;
+		for (auto it = preds.begin(); it != preds.end(); it++) {
+			m_adj[*it].push_back(thisNode);
+		}
+		m_nodeData.push_back(nd);
+		return thisNode;
+	}
+	
+	// Meaning that we will never run out of places to go regardless of
+	// which branches we take
+	bool isRecurrent() {
+		for (auto it = m_adj.begin(); it != m_adj.end(); it++) {
+			if (it->size() == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool isAtDeadEnd() {
+		std::set<int> nodesAt;
+		for (int i = 0; i < m_nodeCount; i++) {
+			if (nodeContains(i, m_vehicle)) {
+				nodesAt.insert(i);
+			}
+		}
+		// No out-edges, except into the current set
+		for (auto it = nodesAt.begin(); it != nodesAt.end(); it++) {
+			for (auto succ = m_adj[*it].begin(); succ != m_adj[*it].end(); succ++) {
+				// One of the waypoints we're in offers a way forward, not stuck here
+				if (nodesAt.find(*succ) == nodesAt.end()) {
+					return false;
+				}
+			}
+		}
+
+		// Recurrent (sub) sets are never dead end.
+		for (auto it = nodesAt.begin(); it != nodesAt.end(); it++) {
+			bool itHasSucc = false;
+			for (auto succ = m_adj[*it].begin(); succ != m_adj[*it].end(); succ++) {
+				if (nodesAt.find(*succ) != nodesAt.end()) {
+					itHasSucc = true;
+					break;
+				}
+			}
+			// One of the nodes we're in doesn't offer a way into any of its neighbors,
+			// so it's not a recurrent (sub) set and thus a dead end
+			if (!itHasSucc) {
+				return true;
+			}
+		}
+		// if we reach this point, must have been a recurrent set, not dead-end
+		return false; 
+	}
+	static const int PRECISION = 10;	
+	static const int SUCCESS = 0;
+	
+	/* Returns index of the node containing the mob. 
+	 * If mob is in multiple nodes, returns the one whose center is closest.
+	 * If in is in no nodes, returns negative int. */
+	int getCurNode() {
+		auto mob = m_vehicle;
+		vector<int> curNodes;
+		for (int i = 0; i < m_nodeCount; i++) {
+			if (nodeContains(i, mob)) {
+				curNodes.push_back(i);
+			}
+		}
+		if (curNodes.size() == 0) {
+			return -1;
+		}
+
+		double minDistSq = std::numeric_limits<double>::infinity();
+		int iMin = -1;
+		for (auto it = curNodes.begin(); it != curNodes.end(); it++) {
+			NodeDatum dat = m_nodeData[*it];
+			auto cx = dat.x, cy = dat.y;
+			auto distSq = (cx - mob.x)*(cx - mob.x) + (cy - mob.y)*(cy - mob.y);
+			if (distSq < minDistSq) {
+				minDistSq = distSq;
+				iMin = *it;
+			}
+		}
+
+		return iMin;
+	}
+
+	void lineTo(double rad, double wpX, double wpY, double mX = std::numeric_limits<double>::quiet_NaN(), double mY = std::numeric_limits<double>::quiet_NaN(), int precision = PRECISION) {
+		// NaN used as sentinel value to represent default behavior, which is to take mob position from the plan's
+		// idea of where the vehicle is.
+		if (std::isnan(mX) || std::isnan(mY)) {
+			mX = m_vehicle.x; mY = m_vehicle.y;
+		}
+		if (wpX == mX || wpY == mY) {
+			return;
+		}
+		auto deltaX = (wpX - mX) / precision;
+		auto deltaY = (wpY - mY) / precision;
+		int curNode = 0;
+		if ((curNode = getCurNode()) < 0) {
+			return;
+		}
+		int i = 0;
+		do {
+			mX += deltaX; mY += deltaY;
+			NodeDatum curDatum = { mX, mY, rad };
+			curNode = addNode(curNode, curDatum);
+		} while (++i < PRECISION);
+	}
+private:
+	int m_nodeCount;
+	vector<NodeDatum> m_nodeData;
+	vector<vector<int>> m_adj;
+	Mob m_vehicle;
+
+	bool nodeContains(int i, Mob m) {
+		NodeDatum dat = m_nodeData[i];
+		auto cx = dat.x, cy = dat.y;
+		auto distSq = (cx - m.x)*(cx - m.x) + (cy - m.y)*(cy - m.y);
+		return dat.rad*dat.rad >= distSq;
+	}
+};
